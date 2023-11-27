7be8ddc55e63b36c92e541ebcb36e1f4d93d0afe
ojknation <ojknation@gmail.com>
2023-11-27 23:36:31 +0100

    fix build error

--- src/reuseables/GlassTint.tsx
+++ src/reuseables/GlassTint.tsx
@@ -3,6 +3,8 @@ import { Box, useStyleConfig } from "@chakra-ui/react"
 function GlassTint(props: { [x: string]: unknown; variant: unknown }) {
   const { variant, ...rest } = props
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   const styles = useStyleConfig("Glass", { variant })
 
   // Pass the computed styles into the `__css` prop
